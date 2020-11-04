d6f7b2b35f4bab164695457661ef1024ffaac82c
goldenbabyy <ventzicalovenia@gmail.com>
2020-11-04 10:45:01 +0700

    recommend 2&3

--- src/app/recommend/recommendation2/recommendation2.page.ts
+++ src/app/recommend/recommendation2/recommendation2.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recommendation2',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Recommendation2Page implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  withInput(event, withInput){
+    console.log(withInput);
+    this.router.navigate(['/recommendation3']);
+  }
 }

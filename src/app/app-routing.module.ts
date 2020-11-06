d9d28961d6799961bb5f5b536877e3b9e25d2ed1
vanessa_ardelia <vanessa.ardelia@gdn-commerce.com>
2020-11-06 22:04:49 +0700

    add router to history page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,6 +31,11 @@ const routes: Routes = [
     path: 'recommendation-result',
     loadChildren: () => import('./recommend/recommendation-result/recommendation-result.module').then( m => m.RecommendationResultPageModule)
   },
+  {
+    path: 'history',
+    loadChildren: () => import('./history/history.module').then( m => m.HistoryPageModule)
+  },
+
 ];
 
 @NgModule({

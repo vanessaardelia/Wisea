e47db1c4bf52225f31f34786ee6e5e63fb5ad757
Benny Richardson <benny.richardson@student.umn.ac.id>
2020-11-18 22:20:50 +0700

    onBoarding Page ok

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -35,7 +35,7 @@ export class AuthService {
       name: userData.name,
       username: userData.username,
       phone: userData.phone,
-      photo: 'default',
+      photo: 'account.svg',
       balance: 0,
     });
   }
